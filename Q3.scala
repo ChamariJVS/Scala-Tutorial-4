97bd66900fdbdd26db6fad32ed2b66bc0fbdb75f
ChamariJVS <93367667+ChamariJVS@users.noreply.github.com>
2022-08-01 20:39:54 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -2,23 +2,34 @@ object Q3 {
 
   def main(args: Array[String]): Unit = {
 
-    formatNames("Benny", toUpper(_));
-    formatNames("Niroshan", toLower(_))
-    formatNames("Saman", toLower(_))
-    formatNames("Kumara", toUpper(_))
+    formatNames("Benny")(toUpper(_))(Array());
+    formatNames("Niroshan")(toLower(_))(Array(0, 1));
+    formatNames("Saman")(toLower(_))(Array());
+    formatNames("Kumara")(toLower(_))(Array(0,5));
 
   }
 
-  def toUpper(name: String): Unit = {
-    println(name.toUpperCase());
+  def toUpper(name: String): String = {
+    name.toUpperCase();
   }
 
-  def toLower(name: String): Unit = {
-    println(name.toLowerCase());
+  def toLower(name: String): String = {
+    name.toLowerCase();
   }
 
-  def formatNames(name: String, function: String => Unit): Unit = {
-    function(name)
+  def formatNames(name: String)(function: String => String)(indexlist: Array[Int]): Unit = {
+    var n = function(name)
+    var index: Int = 0;
+    while (index < indexlist.length) {
+      var x = n.charAt(indexlist(index));
+      n = n.substring(0, indexlist(index)) + x.toUpper + n.substring(
+        indexlist(index) + 1
+      ) 
+      index = index + 1
+    }
+
+    println(n)
+
   }
 
 }
